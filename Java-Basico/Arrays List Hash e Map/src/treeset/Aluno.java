a7a53d45ad50bcdff65e3a7134e97cc734950681
kaio10-collab <sancheskaio@gmail.com>
2025-06-23 13:41:26 -0300

    Estudo sobre TreeSet.

--- Java-Basico/Arrays List Hash e Map/src/treeset/Aluno.java
+++ Java-Basico/Arrays List Hash e Map/src/treeset/Aluno.java
@@ -1,4 +1,23 @@
 package treeset;
 
 public class Aluno {
+
+    public static void main(String[] args) {
+
+        GerenciarAlunos gerenciamento = new GerenciarAlunos();
+
+        gerenciamento.adicionarAluno("Maria");
+        gerenciamento.adicionarAluno("João");
+        gerenciamento.adicionarAluno("Ana");
+        gerenciamento.adicionarAluno("Lucas");
+        gerenciamento.adicionarAluno("João"); // Teste de duplicata
+
+        gerenciamento.listarAlunos();
+
+        gerenciamento.removerAluno("Ana");
+
+        gerenciamento.listarAlunos();
+
+        gerenciamento.listarAlunosEmOrdemAlfabetica();
+    }
 }
--- Java-Basico/Arrays List Hash e Map/src/treeset/TreeSetPrincipal.java
+++ Java-Basico/Arrays List Hash e Map/src/treeset/TreeSetPrincipal.java
@@ -1,4 +1,47 @@
 package treeset;
 
+import java.util.TreeSet;
+
 public class TreeSetPrincipal {
+
+    /*
+
+    */
+
+    public static void main(String[] args) {
+        // Declaração e inicialização de um TreeSet de Strings
+        TreeSet<String> frutas = new TreeSet<>();
+
+        // Adicionando elementos
+        frutas.add("Banana");
+        frutas.add("Pera");
+        frutas.add("Abacaxi");
+        frutas.add("Laranja");
+        frutas.add("Morango");
+        frutas.add("Maçã");
+        frutas.add("Maçã"); // Duplicado, então não será adicionado
+
+        // Imprimindo o TreeSet
+        System.out.println("Frutas (em ordem): " + frutas);
+
+        // Removendo um elemento
+        frutas.remove("Banana");
+        System.out.println("Após remoção de Banana: " + frutas);
+
+        // Verificando se contém um elemento
+        System.out.println("Contém Laranja? " + frutas.contains("Laranja")); // Saída: true
+
+        // Tamanho do TreeSet
+        System.out.println("Tamanho do TreeSet: " + frutas.size()); // 4
+
+        // Iterando sobre o TreeSet
+        System.out.println("Iterando sobre as frutas:");
+        for (String fruta : frutas) {
+            System.out.println(fruta);
+        }
+
+        System.out.println("Primeira fruta: " + frutas.first()); // primeira fruta na ordem
+        System.out.println("Última fruta: " + frutas.last()); // última fruta na ordem
+        System.out.println("Fruta imediatamente maior que 'Laranja': " + frutas.higher("Laranja")); // Saída: Maçã, pois ela captura o próximo valor depois de Laranja.
+    }
 }
